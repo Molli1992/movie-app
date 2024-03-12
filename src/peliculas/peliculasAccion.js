d9178ba81cf6410817458b7a58e1bbeeed37ae37
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-03-12 12:36:11 -0300

    cambiando peliculas

--- src/peliculas/peliculasAccion.js
+++ src/peliculas/peliculasAccion.js
@@ -1,12 +1,48 @@
 //---------------------Peliculas Accion------------------------------------------
 
-
-const arrayAccion = [{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},];
+const arrayAccion = [
+  {
+    titulo: "El Trato",
+    img: "https://pics.filmaffinity.com/El_trato-327912384-large.jpg",
+    descripcion: "Policial",
+    video: "https://www.youtube.com/embed/sevmIeHmcS0",
+  },
+  {
+    titulo: "Equalizers",
+    img: "https://pics.filmaffinity.com/The_Equalizer_El_protector-392015711-large.jpg",
+    descripcion: "Denzel Washington nos regala esta premiada pelucla de accion",
+    video: "https://www.youtube.com/embed/93e44IUHMyA",
+  },
+  {
+    titulo: "Pánico en el Metro",
+    img: "https://i.ytimg.com/vi/0F1x6D-HYIM/sddefault.jpg",
+    descripcion: "Destruccion y terrorismo",
+    video: "https://www.youtube.com/embed/0F1x6D-HYIM",
+  },
+  {
+    titulo: "Carga preciosa",
+    img: "https://es.web.img2.acsta.net/pictures/16/02/24/17/16/355090.jpg",
+    descripcion: "Bruce Willies a pura accion",
+    video: "https://www.youtube.com/embed/XeC9pWg9Wlg",
+  },
+  {
+    titulo: "La Profecía del Juicio Final",
+    img: "https://m.media-amazon.com/images/S/pv-target-images/bff7905c4f3cd218870ea1214c00bf30e273c734dfb87331fc364fbf3f3158f7.jpg",
+    descripcion: "Fin del mundo",
+    video: "https://www.youtube.com/embed/_hNpOVl1qZ0",
+  },
+  {
+    titulo: "Rescate EXTREMO",
+    img: "https://i.ytimg.com/vi/P82L7kTOmTw/maxresdefault.jpg",
+    descripcion: "Militar venganza persecusion",
+    video: "https://www.youtube.com/embed/P82L7kTOmTw",
+  },
+  {
+    titulo: "Más Allá De La Ley",
+    img: "https://pics.filmaffinity.com/Maas_allaa_de_la_ley-768146567-large.jpg",
+    descripcion: "Policial",
+    video: "https://www.youtube.com/embed/8uLtRMnVuRA",
+  },
+];
 
 export default arrayAccion;
--- src/peliculas/peliculasComedia.js
+++ src/peliculas/peliculasComedia.js
@@ -1,12 +1,48 @@
 //---------------------Peliculas Comedia------------------------------------------
 
+const arrayComedia = [
+  {
+    titulo: "Tiempo de Valientes",
+    img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",
+    descripcion: "Pelicula Argentina de comedia y policial",
+    video: "https://www.youtube.com/embed/D8ToTUXxMf4",
+  },
+  {
+    titulo: "Los Marziano",
+    img: "https://pics.filmaffinity.com/Los_Marziano-253436320-large.jpg",
+    descripcion: "Pelicula Argentina de comedia con Franchella",
+    video: "https://www.youtube.com/embed/QgAWv04i2wk",
+  },
+  {
+    titulo: "Infiltrados en la universidad",
+    img: "https://pics.filmaffinity.com/Infiltrados_en_la_universidad-424659797-large.jpg",
+    descripcion: "Accion y comedia",
+    video: "https://www.youtube.com/embed/-oez_qNzIvM",
+  },
+  {
+    titulo: "Superhéroes",
+    img: "https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/67/94/14/20242562.jpg",
+    descripcion: "Comedia y fantasia",
+    video: "https://www.youtube.com/embed/ujPcNoA5um4",
+  },
+  {
+    titulo: "Sólo Amigos",
+    img: "https://es.web.img3.acsta.net/medias/nmedia/18/83/61/93/20064167.jpg",
+    descripcion: "Comedia y romance",
+    video: "https://www.youtube.com/embed/FkMoMz6rLmo",
+  },
+  {
+    titulo: "Un Niño Problemático",
+    img: "https://i.ytimg.com/vi/2GpZ_3OohrU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA3gB_NQbh_pruD62hheKPNVUsJKA",
+    descripcion: "Comedia infantil",
+    video: "https://www.youtube.com/embed/2GpZ_3OohrU",
+  },
+  {
+    titulo: "TRES IDIOTAS",
+    img: "https://pics.filmaffinity.com/3_idiotas-394677796-large.jpg",
+    descripcion: "Comedia y adolescente",
+    video: "https://www.youtube.com/embed/WJiPVZoU-Sk",
+  },
+];
 
-const arrayComedia = [{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},];
-
-export default arrayComedia
+export default arrayComedia;
