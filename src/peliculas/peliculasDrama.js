ef10c3ae6f36d4d5088887a270540b24f64efd36
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-03-12 12:09:06 -0300

    cambiando peliculas

--- src/peliculas/peliculasDrama.js
+++ src/peliculas/peliculasDrama.js
@@ -1,13 +1,50 @@
 //---------------------Peliculas Drama------------------------------------------
 
-
-const arrayDrama = [{titulo: "La Caida", img: "https://www.unicen.edu.ar/iehs/img/filmoteca/la%20caida.jpg",descripcion: "Pelicula Belica y Dramatica sobre la caida de Berlin en el fin de la segunda guerra mundial", video: "https://www.youtube.com/embed/wO1cYOH5yGo"},
-{titulo: "Mas Respeto que soy tu Madre", img: "https://m.media-amazon.com/images/M/MV5BZTBlNGI1YmMtZWRkYi00NWMyLWEyNzQtNjRhNTM3Mjk1MTA2XkEyXkFqcGdeQXVyOTE1NDQ5OA@@._V1_.jpg",descripcion: "Pelicula Argentina Dramatica con la actuacion especial de Diego Peretti", video: "https://www.youtube.com/embed/wSiJXyOBE4g"},
-{titulo: "Codigo Enigma", img: "https://diamondfilms.com/img/poster/5c754ec430c316e507f64e4da.jpg",descripcion: "El gran descubrimiento de Alan Turing", video: "https://www.youtube.com/embed/KrDKN86pgjI"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},];
+const arrayDrama = [
+  {
+    titulo: "La Caida",
+    img: "https://www.unicen.edu.ar/iehs/img/filmoteca/la%20caida.jpg",
+    descripcion:
+      "Pelicula Belica y Dramatica sobre la caida de Berlin en el fin de la segunda guerra mundial",
+    video: "https://www.youtube.com/embed/wO1cYOH5yGo",
+  },
+  {
+    titulo: "Mas Respeto que soy tu Madre",
+    img: "https://m.media-amazon.com/images/M/MV5BZTBlNGI1YmMtZWRkYi00NWMyLWEyNzQtNjRhNTM3Mjk1MTA2XkEyXkFqcGdeQXVyOTE1NDQ5OA@@._V1_.jpg",
+    descripcion:
+      "Pelicula Argentina Dramatica con la actuacion especial de Diego Peretti",
+    video: "https://www.youtube.com/embed/wSiJXyOBE4g",
+  },
+  {
+    titulo: "Codigo Enigma",
+    img: "https://diamondfilms.com/img/poster/5c754ec430c316e507f64e4da.jpg",
+    descripcion: "El gran descubrimiento de Alan Turing",
+    video: "https://www.youtube.com/embed/KrDKN86pgjI",
+  },
+  {
+    titulo: "Luchando por una Nueva Vida",
+    img: "https://i.ytimg.com/vi/FdYsVUsxClI/maxresdefault.jpg",
+    descripcion: "Emocionante y desafiante",
+    video: "https://www.youtube.com/embed/FdYsVUsxClI",
+  },
+  {
+    titulo: "Preciosa",
+    img: "https://miro.medium.com/v2/resize:fit:400/0*bOgLn4i1DpMS3Lbt.jpg",
+    descripcion: "Crecimiento personal",
+    video: "https://www.youtube.com/embed/-DWhVOCDzUo",
+  },
+  {
+    titulo: "Lo mejor de mi vida",
+    img: "https://i.ytimg.com/vi/AF-_qIF8svA/maxresdefault.jpg",
+    descripcion: "Amor y pasion",
+    video: "https://www.youtube.com/embed/yGL1qHUh_I4",
+  },
+  {
+    titulo: "Persecucion Mental",
+    img: "https://m.media-amazon.com/images/S/pv-target-images/7b76e57b2059cec3e1970ef95db5c5ee6ce08589572f6b988c913543f3ffaf12.jpg",
+    descripcion: "Psicologica",
+    video: "https://www.youtube.com/embed/5cOsADfZZGc",
+  },
+];
 
 export default arrayDrama;
-
--- src/peliculas/peliculasFantasia.js
+++ src/peliculas/peliculasFantasia.js
@@ -1,12 +1,48 @@
 //---------------------Peliculas Fantasia------------------------------------------
 
-
- const arrayFantasia = [{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},
-{titulo: "Tiempo de Valientes", img: "https://m.media-amazon.com/images/M/MV5BMDAwZWM1MjYtNjE2NC00MGMxLTg4N2UtNWUxOTY0M2ViMmRkXkEyXkFqcGdeQXVyNzgzODI1OTE@._V1_.jpg",descripcion: "Pelicula Argentina de comedia y policial", video: "https://www.youtube.com/embed/D8ToTUXxMf4"},];
+const arrayFantasia = [
+  {
+    titulo: "Merlin",
+    img: "https://es.web.img2.acsta.net/pictures/19/06/04/12/32/4884987.jpg",
+    descripcion: "Historia del mago Merlin",
+    video: "https://www.youtube.com/embed/xNvXX9HZ_gU",
+  },
+  {
+    titulo: "Joven manos de tijera",
+    img: "https://mx.web.img2.acsta.net/pictures/17/08/14/20/17/217767.jpg",
+    descripcion: "Jhonny Deep nos apaciona en esta increible fantasia",
+    video: "https://www.youtube.com/embed/qMdvmZ6t4wA",
+  },
+  {
+    titulo: "Magia extraña",
+    img: "https://play-lh.googleusercontent.com/RzowwL97I0PPUFG0oTEv-bG_QGwW93bm8vEixVIJ2BfA-fz1c0gN6WpSq0urUq4_pCJQ",
+    descripcion: "Intensa y apasionante",
+    video: "https://www.youtube.com/embed/jH8YctjH-Cs",
+  },
+  {
+    titulo: "Reloj Mágico",
+    img: "https://i.ytimg.com/vi/rOTNundcXYQ/maxresdefault.jpg",
+    descripcion: "Familiar y divertida",
+    video: "https://www.youtube.com/embed/rOTNundcXYQ",
+  },
+  {
+    titulo: "ABIGAIL",
+    img: "https://i.ytimg.com/vi/ZdH4MVd5mbQ/maxresdefault.jpg",
+    descripcion: "Accion y amistad",
+    video: "https://www.youtube.com/embed/YNSQs700izk",
+  },
+  {
+    titulo: "Reino de Ágonos",
+    img: "https://i.ytimg.com/vi/zBc2s8sZuuk/maxresdefault.jpg",
+    descripcion: "Magia y accion",
+    video: "https://www.youtube.com/embed/zBc2s8sZuuk",
+  },
+  {
+    titulo: "MUNDO PERDIDO",
+    img: "https://i.ytimg.com/vi/TZutM1xLFkM/maxresdefault.jpg",
+    descripcion: "Exploracion y aventura",
+    video: "https://www.youtube.com/embed/TZutM1xLFkM",
+  },
+];
 
 export default arrayFantasia;
